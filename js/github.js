2011594fc23b49dc477cbdca6822a6801c211ef4
Maldon <maldonmeehan@gmail.com>
2016-08-05 08:33:52 -0700

    add starter code

--- js/github.js
+++ js/github.js
@@ -1,1 +1,9 @@
 var apiKey = require('./../.env').apiKey;
+
+exports.getRepos = function(){
+  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+    console.log(response);
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+};
