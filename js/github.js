9e516bd0f57a9c7da70ba7e158eea3f95d6d54a9
Maldon <maldonmeehan@gmail.com>
2016-08-05 12:34:45 -0700

    add number of repos

--- js/github.js
+++ js/github.js
@@ -3,12 +3,14 @@ var apiKey = require('./../.env').apiKey;
 exports.Repo = function(){
 };
 
-exports.Repo.prototype.getRepos = function(userName){
+exports.Repo.prototype.getRepos = function(userName, public_repos){
   $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response){
     $('.showUserName').empty();
     $('.showUserName').append('<h3>Username: '  + response.login + '</h3>');
     $('.showImg').empty();
     $('.showImg').append("<img src=" + response.avatar_url + ">");
+    $('.showNumberOfRepos').empty();
+    $('.showNumberOfRepos').append('<h3>Number of Repos: ' + response.public_repos + '</h3>');
   }).fail(function(error){
 
   });
