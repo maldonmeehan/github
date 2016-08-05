d56705a6516b092f2464cbd328e200a431c1b02d
Maldon <maldonmeehan@gmail.com>
2016-08-05 12:01:33 -0700

    add working repo display

--- js/github.js
+++ js/github.js
@@ -11,4 +11,12 @@ exports.Repo.prototype.getRepos = function(userName){
   }).fail(function(error){
     // console.log(error.responseJSON.Repo);
   });
+
+  $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(response){
+    for(var i = 0; i < response.length; i++){
+      $('.showRepos').append('<h3>' + 'Repo name: ' + response[i].name + '</h4><p>' + response[i].description + ' <a href="' + response[i].html_url + '"><br> <h4> Repo Link </h4> </a></p>');
+    }
+    $('.showRepos').append('</ol>');
+  }).fail(function(error){
+  });
 };
